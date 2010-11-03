b343d7cef948379fc328a38de766df2f0b6f3448
Irina Dumitrascu <me@dira.ro>
2010-11-02 21:12:25 -0400

    Bookmarkable colored buttons

--- public/javascripts/generate.js
+++ public/javascripts/generate.js
@@ -1,14 +1,28 @@
 $(document).ready(init);
 
 function init() {
-  $('input[type=submit]').click(generate);
+  submit_field().click(generate);
+
+  color = document.location.hash.substr(1);
+  if (color != "") {
+    color_field().val(color);
+  }
+  submit_field().click();
+}
+
+function submit_field() {
+  return $('input[type=submit]')
+}
+
+function color_field() {
+  return $('input[name=color]')
 }
 
 function generate(e) {
   e.stopPropagation();
   e.preventDefault();
 
-  color = $('input[name=color]').val();
+  color = color_field().val();
   form = $('form');
   $.ajax({
     type: form.attr('method'),
@@ -25,6 +39,7 @@ function reset() {
 }
 function success(html) {
   response(html);
+  document.location.hash = color_field().val();
 }
 function error() {
   response('An error occurred. Please try again');
